2ddd66a50e8a3a4093fce1bb7a8613eaaaa8cf83
unknown <mgaspar@nsign.tv>
2021-11-23 17:45:06 +0100

    First test of call of football api

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,53 @@
-async function request(url, verb) {
-    try {
-        const result = await fetch(url, {
-            method: verb,
-        });
-        const data = await result.json();
-        if (result.status !== 200) {
-            return false;
+async function request(verb, url, data) {
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    console.log('Sending data');
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
         }
-        return data;
-    } catch (error) {
-        console.error(error.message);
-        return false;
+    });
+
+    xhr.open(verb, 'https://v3.football.api-sports.io/teams?country=Spain&league=140&season=2020');
+
+    xhr.setRequestHeader('Authorization', null);
+    xhr.setRequestHeader('x-rapidapi-key', 'ebfa151c4637db0f313a47b7489e3770');
+    xhr.setRequestHeader('x-rapidapi-host', 'v3.football.api-sports.io');
+
+    if (data) {
+        console.log('Sending data');
+        xhr.send();
+    } else {
+        xhr.send();
     }
 }
+
+async function load() {
+    const dataToPass = {
+        country: 'Spain',
+        league: 140,
+        season: 2020,
+    };
+    const data = await request('GET', 'https://v3.football.api-sports.io/teams', dataToPass);
+    console.log(data);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    load();
+});
+
+// async function request(url, verb) {
+//     try {
+//         const result = await fetch(url, {
+//             method: verb,
+//         });
+//         const data = await result.json();
+//         if (result.status !== 200) {
+//             return false;
+//         }
+//         return data;
+//     } catch (error) {
+//         console.error(error.message);
+//         return false;
+//     }
+// }
