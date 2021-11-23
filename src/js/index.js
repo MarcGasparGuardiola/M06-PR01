512340821f4ad3fb9dfa9f0dc1000e59748f781b
unknown <mgaspar@nsign.tv>
2021-11-23 19:03:41 +0100

    Added doRequest function

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,44 @@
-async function request(verb, url, data) {
-    const xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-    console.log('Sending data');
-    xhr.addEventListener('readystatechange', function () {
-        if (this.readyState === 4) {
-            console.log(this.responseText);
+/* eslint-disable max-len */
+/* eslint-disable prefer-template */
+async function doRequest(url, params, verb, jsonResponse) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('readystatechange', () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var response = (xhr.responseText && (xhr.responseText.length > 0)) ? xhr.responseText : true;
+                if ((jsonResponse === undefined) || jsonResponse) {
+                    response = JSON.parse(xhr.responseText);
+                }
+                return resolve(response);
+            } if (xhr.readyState == 4) {
+                var response = (xhr.responseText && (xhr.responseText.length > 0)) ? xhr.responseText : false;
+                if ((jsonResponse === undefined) || jsonResponse) {
+                    response = JSON.parse(xhr.responseText);
+                }
+                return reject(response);
+            }
+        });
+        let finalUrl = url;
+        if (verb === 'GET') {
+            if (params) {
+                const stringParams = Object.keys(params).map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&');
+
+                if (stringParams.length > 0) {
+                    finalUrl = url + '?' + stringParams;
+                }
+            }
+        }
+        xhr.open(verb, finalUrl);
+        //xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        xhr.setRequestHeader("x-rapidapi-key", "ebfa151c4637db0f313a47b7489e3770");
+        xhr.setRequestHeader("x-rapidapi-host", "v3.football.api-sports.io");
+        if (verb === 'POST') {
+            const data = JSON.stringify(params);
+            xhr.send(data);
+        } else {
+            xhr.send();
         }
     });
-
-    xhr.open(verb, 'https://v3.football.api-sports.io/teams?country=Spain&league=140&season=2020');
-
-    xhr.setRequestHeader('Authorization', null);
-    xhr.setRequestHeader('x-rapidapi-key', 'ebfa151c4637db0f313a47b7489e3770');
-    xhr.setRequestHeader('x-rapidapi-host', 'v3.football.api-sports.io');
-
-    if (data) {
-        console.log('Sending data');
-        xhr.send();
-    } else {
-        xhr.send();
-    }
 }
 
 async function load() {
@@ -28,7 +47,7 @@ async function load() {
         league: 140,
         season: 2020,
     };
-    const data = await request('GET', 'https://v3.football.api-sports.io/teams', dataToPass);
+    const data = await doRequest('https://v3.football.api-sports.io/teams', dataToPass, 'GET');
     console.log(data);
 }
 
