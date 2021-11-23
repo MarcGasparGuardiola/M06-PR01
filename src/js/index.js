efa7b415879ddf793318bfee7f0edf3b57614cf3
Alv-ar <77983145+Alv-ar@users.noreply.github.com>
2021-11-23 16:55:10 +0100

    added eslint

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+async function request(url, verb) {
+    try {
+        const result = await fetch(url, {
+            method: verb,
+        });
+        const data = await result.json();
+        if (result.status !== 200) {
+            return false;
+        }
+        return data;
+    } catch (error) {
+        console.error(error.message);
+        return false;
+    }
+}
