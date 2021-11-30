e83e69b5c97b64b89964ab19469dbfae5b14b29a
Alv-ar <77983145+Alv-ar@users.noreply.github.com>
2021-11-30 17:46:34 +0100

    fix: validation errors

--- src/js/login/login.js
+++ src/js/login/login.js
@@ -11,18 +11,26 @@ let users = [];
 const regex = {
     userName: /^[A-Za-z ]+$/,
     email: /^\w+([\.\+\-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/,
-    password: /^[A-Za-z0-9]{5}$/,
+    password: /^[A-Za-z0-9]{5,}$/,
 }
 
 function isUserName(userName) {return regex.userName.test(userName)}
 function isEmail(email) {return regex.email.test(email)}
 function isPassword(password) {return regex.password.test(password)}
+//TODO validacio tot el formulari
+function allValid() {
+    for (let index = 0; index < forms.length; index++) {
+        if (forms[index].className.includes("is-valid")) {
+            console.log("ok")
+        }
+    }
+}
 
 function setGreen(input) {
-    return input.classList.add("is-valid");
+    input.className = "form-control is-valid";
 }
 function setRed(input) {
-    return input.classList.add("is-invalid");
+    input.className = "form-control is-invalid";
 }
 
 forms.addEventListener("keyup", (e) => {
@@ -40,7 +48,9 @@ forms.addEventListener("keyup", (e) => {
             isPassword(e.target.value) ? setGreen(e.target) : setRed(e.target)
         default:
             break;
+
     }
+    allValid()
 })
 
 
@@ -52,6 +62,7 @@ submit.addEventListener('click', (e) => {
     users.push(user)
     console.log(users)
     sessionStorage.setItem('usersArray', JSON.stringify(users))
+    window.location.href = '../../loadplayer.html'
 })
 
 
